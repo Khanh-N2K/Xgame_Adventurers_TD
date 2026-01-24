259b0b01673158b045296031b31afc4cd49f3b44
LuongSon28112004 <sonluong165@gmail.com>
2026-01-24 11:01:47 +0700

    update

--- Assets/Scripts/MapCtr.cs
+++ Assets/Scripts/MapCtr.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapCtr : Singleton<MapCtr>
 {
     public Base[] enemyPrefab;
     public Base[] characterPrefab;
+
     public List<Base> listEnemys = new();
     public List<Base> listCharacters = new();
+
     public Transform characterTransform;
     public Transform enemyTransform;
-    private int currentWave = 0;
+
     public SpawnSO spawnSOData;
-    private float[] PosZ = new float[11] { -0.125f, -0.1f, -0.075f, -0.05f, -0.025f, 0, 0.025f, 0.05f, 0.075f, 0.1f, 0.125f };
+
+    private int currentWave = 0;
+
+    [Header("Spawn Setting")]
+    [SerializeField] private int maxPerRow = 5;
+    [SerializeField] private float spacingZ = 0.03f;
+    [SerializeField] private float spacingX = 0.08f;
+    [SerializeField] private float enemyStartX = 1f;
+    [SerializeField] private float characterStartX = -0.55f;
+    [SerializeField] private float posY = 0.03f;
+
+    private int enemySpawnIndex = 0;
+    private int characterSpawnIndex = 0;
 
     private void Start()
     {
@@ -24,177 +36,190 @@ public class MapCtr : Singleton<MapCtr>
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
-        {
             SpawnCharacter(0);
-        }
 
         if (Input.GetKeyDown(KeyCode.D))
-        {
             SpawnCharacter(1);
-        }
 
         if (Input.GetKeyDown(KeyCode.W))
-        {
             SpawnCharacter(2);
-        }
     }
+
+    #region ==== GET CLOSEST ====
 
     public Base GetClosestEnemy(Vector3 root)
     {
-        if (listEnemys.Count == 0)
-        {
-            return null;
-        }
+        Base closest = null;
+        float minDis = float.MaxValue;
 
-        Base closestCharacter = listEnemys[0];
-        float minDistance = Vector3.SqrMagnitude(closestCharacter.transform.position - root);
-        for (int i = 1; i < listEnemys.Count; i++)
+        foreach (var enemy in listEnemys)
         {
-            if (listEnemys[i].status != Status.Die
-                && Vector3.SqrMagnitude(listEnemys[i].transform.position - root) < minDistance)
+            if (enemy.status == Status.Die) continue;
+
+            float dis = Vector3.SqrMagnitude(enemy.transform.position - root);
+            if (dis < minDis)
             {
-                minDistance = Vector3.SqrMagnitude(listEnemys[i].transform.position - root);
-                closestCharacter = listEnemys[i];
+                minDis = dis;
+                closest = enemy;
             }
         }
-        return closestCharacter;
+        return closest;
     }
 
     public Base GetClosestCharacter(Vector3 root)
     {
-        if (listCharacters.Count == 0)
-        {
-            return null;
-        }
+        Base closest = null;
+        float minDis = float.MaxValue;
 
-        Base closestCharacter = listCharacters[0];
-        float minDistance = Vector3.SqrMagnitude(closestCharacter.transform.position - root);
-        for (int i = 1; i < listCharacters.Count; i++)
+        foreach (var character in listCharacters)
         {
-            if (listCharacters[i].status != Status.Die
-                && Vector3.SqrMagnitude(listCharacters[i].transform.position - root) < minDistance)
+            if (character.status == Status.Die) continue;
+
+            float dis = Vector3.SqrMagnitude(character.transform.position - root);
+            if (dis < minDis)
             {
-                minDistance = Vector3.SqrMagnitude(listCharacters[i].transform.position - root);
-                closestCharacter = listCharacters[i];
+                minDis = dis;
+                closest = character;
             }
         }
-        return closestCharacter;
+        return closest;
     }
+
+    #endregion
+
+    #region ==== SPAWN CHARACTER ====
+
+    public void SpawnCharacter(int id)
+    {
+        Base character = CreateCharacterObject(id, characterTransform);
+        if (character == null) return;
+
+        character.SwitchStatus(Status.Attack);
+        listCharacters.Add(character);
+    }
+
+    #endregion
+
+    #region ==== SPAWN ENEMY ====
 
     public void SpawnEnemy()
     {
         StartCoroutine(SpawnEnemyWave(currentWave));
     }
-    public bool CheckEndWave()
-    {
-        bool resuit = true;
-        for (int i = 0; i < listEnemys.Count; i++)
-        {
-            if (listEnemys[i].status != Status.Die)
-            {
-                return false;
-            }
-        }
-
-        for (int i = 0; i < listCharacters.Count; i++)
-        {
-            if (listCharacters[i].status != Status.Die)
-            {
-                return false;
-            }
-        }
-
-        // tăng wave lên
-        currentWave++;
-
-        return resuit;
-    }
-    public void SpawnCharacter(int id)
-    {
-        Base Character = CreateCharacterObject(id, characterTransform);
-        Character.SwitchStatus(Status.Attack);
-        if (Character == null) return;
-        listCharacters.Add(Character);
-    }
-
-    private Base GetEnemyById(int id)
-    {
-        for (int i = 0; i < enemyPrefab.Length; i++)
-        {
-            if (i == id)
-            {
-                return enemyPrefab[i];
-            }
-        }
-        return null;
-    }
-
-    private Base GetCharacterById(int id)
-    {
-        for (int i = 0; i < characterPrefab.Length; i++)
-        {
-            if (i == id)
-            {
-                return characterPrefab[i];
-            }
-        }
-        return null;
-    }
 
     public IEnumerator SpawnEnemyWave(int index)
     {
+        enemySpawnIndex = 0;
+        characterSpawnIndex = 0;
+
         InfoWave infoWave = spawnSOData.infoSpawn.infoWave[index];
-        yield return new WaitForSeconds(infoWave.timeDelay );
+        yield return new WaitForSeconds(infoWave.timeDelay);
+
         for (int i = 0; i < infoWave.infoTurns.Length; i++)
         {
             InfoTurn infoTurn = infoWave.infoTurns[i];
+
             for (int j = 0; j < infoTurn.enemy.Length; j++)
             {
                 Base enemy = CreateEnemyObject(infoTurn.enemy[j], enemyTransform);
                 listEnemys.Add(enemy);
             }
+
             yield return new WaitForSeconds(infoTurn.timedelay);
         }
+    }
+
+    #endregion
+
+    #region ==== CREATE OBJECT ====
+
+    private Vector3 GetEnemySpawnPos()
+    {
+        int row = enemySpawnIndex / maxPerRow;
+        int col = enemySpawnIndex % maxPerRow;
+
+        float x = enemyStartX + row * spacingX;
+        float z = (col - (maxPerRow - 1) / 2f) * spacingZ;
+
+        enemySpawnIndex++;
+        return new Vector3(x, posY, z);
+    }
+
+    private Vector3 GetCharacterSpawnPos()
+    {
+        int row = characterSpawnIndex / maxPerRow;
+        int col = characterSpawnIndex % maxPerRow;
+
+        float x = characterStartX - row * spacingX;
+        float z = (col - (maxPerRow - 1) / 2f) * spacingZ;
+
+        characterSpawnIndex++;
+        return new Vector3(x, posY, z);
     }
 
     public Base CreateEnemyObject(int id, Transform parent)
     {
         Base enemy = GetEnemyById(id);
         if (enemy == null) return null;
-        int idx = Random.Range(0, PosZ.Length);
-        Vector3 pos = new Vector3(1f, 0.03f, PosZ[idx]);
+
         Base newEnemy = Instantiate(enemy, parent);
-        newEnemy.transform.position = pos;
+        newEnemy.transform.position = GetEnemySpawnPos();
         newEnemy.transform.rotation = Quaternion.identity;
         return newEnemy;
     }
 
     public Base CreateCharacterObject(int id, Transform parent)
     {
-        Base Character = GetCharacterById(id);
-        if (Character == null) return null;
-        int idx = Random.Range(0, PosZ.Length);
-        Vector3 pos = new Vector3(-0.55f, 0.03f, PosZ[idx]);
-        Base newCharacter = Instantiate(Character, parent);
-        newCharacter.transform.position = pos;
+        Base character = GetCharacterById(id);
+        if (character == null) return null;
+
+        Base newCharacter = Instantiate(character, parent);
+        newCharacter.transform.position = GetCharacterSpawnPos();
         newCharacter.transform.rotation = Quaternion.identity;
         return newCharacter;
     }
 
-    #region ___ CONTEXT MENU ___
+    #endregion
+
+    #region ==== UTILITY ====
+
+    private Base GetEnemyById(int id)
+    {
+        if (id < 0 || id >= enemyPrefab.Length) return null;
+        return enemyPrefab[id];
+    }
+
+    private Base GetCharacterById(int id)
+    {
+        if (id < 0 || id >= characterPrefab.Length) return null;
+        return characterPrefab[id];
+    }
+
+    public bool CheckEndWave()
+    {
+        foreach (var e in listEnemys)
+            if (e.status != Status.Die) return false;
+
+        foreach (var c in listCharacters)
+            if (c.status != Status.Die) return false;
+
+        currentWave++;
+        return true;
+    }
+
+    #endregion
+
+    #region ==== CONTEXT MENU ====
 
     [ContextMenu("Test All Attack")]
     private void TestAllAttack()
     {
         foreach (var enemy in listEnemys)
-        {
             enemy.SwitchStatus(Status.Attack);
-        }
+
         foreach (var character in listCharacters)
-        {
             character.SwitchStatus(Status.Attack);
-        }
     }
 
-    #endregion ___
+    #endregion
 }
